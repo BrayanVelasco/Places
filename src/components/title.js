f16854c5e6a5bde9a841deb48bfdb9dffae12918
BrianVelasco <bryan72vel@hotmail.com>
2019-01-24 08:43:40 -0500

    Example React Web

---
+++ src/components/title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Title extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Places</h1>
+                <p>Discover places in a simple way</p>
+            </div>
+            
+        )
+    }
+} 
---
+++ src/request/places.js
@@ -0,0 +1,20 @@
+export default{
+    places:[
+        {
+            imagenUrl:'/images/place-3.jpg',
+            title:"Juan Valdez Norte",
+            description:"Juan Valdez Café is a multinational coffeehouse chain based in Colombia that specializes in coffee retail. Its purpose is to become a member of the worldwide coffee restaurant business, and promote Colombian coffee."
+
+        },
+        {
+            imagenUrl:'/images/place-1.jpg',
+            title:"Juan Valdez Sur",
+            description:"Juan Valdez Café is a multinational coffeehouse chain based in Colombia that specializes in coffee retail. Its purpose is to become a member of the worldwide coffee restaurant business, and promote Colombian coffee."
+        },
+        {
+            imagenUrl:'/images/place-2.jpg',
+            title:"Juan Valdez Centro Mayor",
+            description:"Juan Valdez Café is a multinational coffeehouse chain based in Colombia that specializes in coffee retail. Its purpose is to become a member of the worldwide coffee restaurant business, and promote Colombian coffee."
+        }
+    ]
+}
